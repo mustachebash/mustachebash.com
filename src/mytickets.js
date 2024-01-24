182c647bdc05a0078d4b6b693b62f23d81e7bf47
Joe Furfaro <joe@mrstache.io>
2024-01-23 22:41:45 -0800

    tickets page refactoring to accomodate extra info and different payload

--- src/mytickets.js
+++ src/mytickets.js
@@ -52,67 +52,124 @@ if(orderToken) {
 			return response;
 		})
 		.then(response => response.json())
-		.then(({customer, order, tickets}) => {
+		.then(({customer, tickets}) => {
 			if(!tickets.length) throw new Error('No tickets returned');
 
-			// UUID alpha sorting should be sufficiently deterministic
-			tickets.sort((a, b) => {
-				if(a.eventDate > b.eventDate) return 1;
-				if(a.eventDate < b.eventDate) return -1;
+			const eventsById = {};
 
-				return a.id > b.id ? 1 : -1;
+			tickets.forEach(ticket => {
+				const { eventId, eventName, eventDate } = ticket;
+				if(!eventsById[eventId]) {
+					const eventDateObj = new Date(eventDate);
+
+					const dateString = eventDateObj.toLocaleDateString('en-US', {
+							weekday: 'long',
+							year: 'numeric',
+							month: 'long',
+							day: 'numeric'
+						}),
+						timeString = eventDateObj.toLocaleTimeString('en-US', {
+							hour: 'numeric',
+							minute: 'numeric',
+							hour12: true
+						});
+
+					eventsById[eventId] = {
+						id: eventId,
+						name: eventName,
+						dateString,
+						timeString,
+						tickets: []
+					};
+				}
+
+				eventsById[eventId].tickets.push(ticket);
 			});
 
-			const { firstName, lastName, email } = customer,
-				{ eventName, eventDate } = tickets[0],
-				confirmationId = order.id.slice(0, 8),
-				eventDateObj = new Date(eventDate);
-
-			// Gets a UTC time, hardcode the PDT offset
-			eventDateObj.setUTCHours(eventDateObj.getUTCHours() - 8);
-
-			// eslint-disable-next-line no-unused-vars
-			const dateString = eventDateObj.toLocaleDateString('en-US', {
-					weekday: 'long',
-					year: 'numeric',
-					month: 'long',
-					day: 'numeric',
-					timeZone: 'UTC'
-				}),
-				eventHour = eventDateObj.getUTCHours() > 12 ? eventDateObj.getUTCHours() - 12 : eventDateObj.getUTCHours(),
-				eventMinutes = `${eventDateObj.getUTCMinutes() < 10 ? `0${eventDateObj.getUTCMinutes()}` : eventDateObj.getUTCMinutes()}`,
-				eventPeriod = eventDateObj.getUTCHours() >= 12 ? 'pm' : 'am',
-				// eslint-disable-next-line
-				timeString = `${eventHour}:${eventMinutes}${eventPeriod}`;
+			// TODO: loop the tickets for QR codes, but display each guest as a row and tier below
+			const { firstName, lastName, email } = customer;
 
 			const ticketsHTML = `
-				<h1>Your tickets</h1>
-				<h2>Order #${confirmationId}</h2>
-				<h2>Purchased by ${firstName} ${lastName} - ${email}</h2>
-				<h4>${eventName}</h4>
-				<h4>${dateString}</h4>
-				<p class="download-wallet">
-					<!-- <a class="wallet" href="#"><img src="./img/apple-wallet.svg" /></a> -->
-				</p>
-				<p class="disclaimer">
-					This is event is 21+ only. All guests must have a valid ID and ticket to check in. Do not share this link
-					with anyone. <!-- If you have any additional questions, please <a href="/info" target="_blank">visit our FAQs</a>.-->
-				</p>
-				<div class="tickets swiper-container">
-					<div class="swiper-wrapper">
-						${tickets.map(({ qrCode, eventName, admissionTier }, i) => (`
-							<div class="ticket swiper-slide">
-								<div class="img-wrap ${!qrCode ? 'outline' : ''}">
-									${qrCode ? `<img src="${qrCode}" />` : '<h5>Your Tickets will be shown here<br>closer to the event</h5>'}
+				<div class="container-1230">
+					<h4>Active tickets</h4>
+					<h3>${firstName} ${lastName}</h3>
+					<h2>${email}</h2>
+					<p class="download-wallet">
+						<!-- <a class="wallet" href="#"><img src="./img/apple-wallet.svg" /></a> -->
+					</p>
+					<p class="disclaimer">
+						This is event is 21+ only. All guests must have a valid ID and ticket to check in. Do not share this link
+						with anyone.<!-- If you have any additional questions, please <a href="/info" target="_blank">visit our FAQs</a>.-->
+					</p>
+					<p class="instructions">
+						Swipe to scan different tickets.<br class="mobile-only" /> Scroll down for more ticket details.
+					</p>
+					<div class="tickets swiper-container">
+						<div class="swiper-wrapper">
+							${tickets.map(({ qrCode, eventName, admissionTier }, i) => (`
+								<div class="ticket swiper-slide">
+									<div class="img-wrap ${!qrCode ? 'outline' : ''}">
+										${qrCode ? `<img src="${qrCode}" />` : '<h5>Your Tickets will be shown here<br>closer to the event</h5>'}
+									</div>
+									<p>${eventName}${admissionTier === 'vip' ? ' &#128378;' : ''}</p>
+									<p>${i + 1}/${tickets.length}</p>
 								</div>
-								<p>${eventName}${admissionTier === 'vip' ? ' &#128378;' : ''}</p>
-								<p>${i + 1}/${tickets.length}</p>
-							</div>
-						`)).join('\n')}
+							`)).join('\n')}
+						</div>
+						<div class="swiper-pagination"></div>
+						<div class="swiper-button-next"></div>
+						<div class="swiper-button-prev"></div>
 					</div>
-					<div class="swiper-pagination"></div>
-					<div class="swiper-button-next"></div>
-					<div class="swiper-button-prev"></div>
+				</div>
+				<div class="tickets-list">
+					<div class="container-1230">
+						${/* eslint-disable indent*/
+						Object.values(eventsById).map(({ name: eventName, dateString, timeString, tickets: eventTickets }) => `
+							<div class="event-group">
+								<div class="event-info">
+									<h4>${eventName}</h4>
+									<p>${dateString} at ${timeString}</p>
+								</div>
+								<ul>
+								${[...eventTickets]
+									.sort((a, b) =>  {
+										// Rank by tier first
+										if(a.admissionTier === 'vip' && b.admissionTier !== 'vip') return -1;
+										if(a.admissionTier !== 'vip' && b.admissionTier === 'vip') return 1;
+
+										return a.id > b.id ? 1 : -1;
+									})
+									.map((ticket, i) => {
+										const { id, orderId, admissionTier } = ticket,
+											ticketId = id.slice(0, 8),
+											confirmationId = orderId.slice(0, 8);
+
+										return `
+											<li class="${admissionTier === 'vip' ? 'vip-tier' : 'general-tier'}">
+												<div class="info">
+													<div class="ticket-number">
+														<h2>#${i + 1}</h2>
+													</div>
+													<div class="ticket-tier">
+														<h5>${admissionTier === 'vip'
+																? 'VIP &#128378;'
+																: window.innerWidth > 420
+																	? 'General Admission'
+																	: 'GA'
+														}</h5>
+													</div>
+													<div class="ticket-ids">
+														<p>Ticket #${ticketId}</p>
+														<p>Confirmation #${confirmationId}</p>
+													</div>
+												</div>
+											</li>
+										`;
+							/* eslint-enable */}).join('\n')}
+								</ul>
+							</div>
+						`).join('\n')}
+					</div>
 				</div>
 			`;
 
