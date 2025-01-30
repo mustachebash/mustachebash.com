88c5b4e6bb1575a734d5110310f9bc916228da60
Joe Furfaro <joe@mrstache.io>
2025-01-29 22:27:33 -0800

    added react framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,44 @@
 import { defineConfig, envField } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mustachebash.com',
 	trailingSlash: 'never',
+
 	experimental: {
-		responsiveImages: true
+			responsiveImages: true
 	},
+
 	server: {
-		host: '0.0.0.0'
+			host: true
 	},
+
 	build: {
-		assets: 'assets'
+			assets: 'assets'
 	},
+
 	env: {
-		schema: {
-			API_HOST: envField.string({context: 'client', access: 'public'}),
-			BRAINTREE_TOKEN: envField.string({context: 'client', access: 'public'})
-		}
-	},
-	vite: {
-		build: {
-			sourcemap: true,
-			rollupOptions: {
-				output: {
-					sourcemapBaseUrl: 'https://mustachebash.com/assets/'
-				}
+			schema: {
+					API_HOST: envField.string({context: 'client', access: 'public'}),
+					BRAINTREE_TOKEN: envField.string({context: 'client', access: 'public'})
 			}
-		}
-	}
+	},
+
+	vite: {
+			build: {
+					sourcemap: true,
+					rollupOptions: {
+							output: {
+									sourcemapBaseUrl: 'https://mustachebash.com/assets/'
+							}
+					}
+			},
+			server: {
+				allowedHosts: true
+			}
+	},
+
+	integrations: [react()]
 });
